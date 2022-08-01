41cf68b09e79f4f76b271d2482ac389573dc43a0
Jungmin-Noh <lumill925@gmail.com>
2022-08-01 17:33:02 +0900

    01. 프로젝트 생성

---
+++ src/test/java/com/sparta/blogwebservice/BlogWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.blogwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
