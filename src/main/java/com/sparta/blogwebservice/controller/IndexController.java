ae735f1e342865d9a07c0915b04af701e6ad506c
Jungmin-Noh <lumill925@gmail.com>
2022-08-02 15:23:34 +0900

    08. html 구조 수정

--- src/main/java/com/sparta/blogwebservice/controller/IndexController.java
+++ src/main/java/com/sparta/blogwebservice/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.sparta.blogwebservice.controller;
 
+import com.sparta.blogwebservice.domain.CommentsRepository;
+import com.sparta.blogwebservice.domain.PostsRepository;
+import com.sparta.blogwebservice.service.CommentsService;
 import com.sparta.blogwebservice.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,10 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     private final PostsService postsService;
+    private final CommentsService commentsService;
+    private final PostsRepository postsRepository;
+    private final CommentsRepository commentsRepository;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute("posts", postsRepository.findAllByOrderByModifiedDateDesc());
 
         return "views/index";
 
